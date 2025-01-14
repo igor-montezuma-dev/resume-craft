0603022c9bc798a253b61d4be44afe852a427d07
IgorMontezuma20 <igorminranda17@gmail.com>
2025-01-14 00:16:20 -0300

    feat: persisting the logged in user in the db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,11 @@
+import { db } from "@/db/drizzle";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [GitHub, Google],
   pages: {
     signIn: "/auth/login",
