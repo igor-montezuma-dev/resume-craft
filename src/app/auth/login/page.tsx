2d3f82a2dfa56d04f35ae617189628d06aadc16d
IgorMontezuma20 <igorminranda17@gmail.com>
2025-01-13 21:04:15 -0300

    feat: Add authentication

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,20 @@
 import Logo from "@/assets/logo.svg";
 import { ThemeToggle } from "@/components/shared/theme-toggle";
 import { Button } from "@/components/ui/button";
+import { signIn } from "@/lib/auth";
 import { Chrome, Github } from "lucide-react";
 import Image from "next/image";
 
+type Providers = "github" | "google";
+
 export default function LoginPage() {
+  const handleLogin = async (form: FormData) => {
+    "use server";
+    const provider = form.get("provider") as Providers;
+
+    await signIn(provider, { redirectTo: "/dashboard/resumes" });
+  };
+
   return (
     <div className="grid grid-cols-[1.5fr,1fr] h-screen">
       <aside>
@@ -17,7 +27,7 @@ export default function LoginPage() {
           quality={100}
         />
       </aside>
-      <form className="flex flex-col p-10 justify-center">
+      <form className="flex flex-col p-10 justify-center" action={handleLogin}>
         <div className="flex items-center justify-between mb-10">
           <Logo className="max-w-[90px]" />
           <ThemeToggle />
